2ca0efda75643e66128749029d4d4bcce6bae3ef
kisleyj <jeffersonkisley@gmail.com>
2025-02-20 16:55:40 -0300

    Subtração e alteração na função

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+// Prototipo da funcao
+int soma(int a, int b);
+int subtrair(int a, int b);
+
 int main() {
     int num1, num2;
-    
-    // Solicita entrada do usuario
     printf("Digite o primeiro numero: ");
     scanf("%d", &num1);
     printf("Digite o segundo numero: ");
     scanf("%d", &num2);
-
-    int resultado = num1 + num2;
-    printf("Resultado da soma: %d\n", resultado);
-    return 0;
+    
+    // Soma
+    printf("Soma: %d\n", soma(num1, num2));
+    // Subtracao
+printf("Subtracao: %d\n", subtrair(num1, num2));
+return 0;
+}
+// Implementacao da funcao
+int soma (int a, int b) {
+    return a + b;
+}
+int subtrair(int a, int b) {
+    return a - b;
 }
