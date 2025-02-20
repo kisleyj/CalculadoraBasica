b10ab33165a30cc59c86bdf4f4f357b150222e21
kisleyj <jeffersonkisley@gmail.com>
2025-02-20 16:58:50 -0300

    Divisão e multiplicação

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,27 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Prototipo da funcao
+// Protótipos das funções
 int soma(int a, int b);
 int subtrair(int a, int b);
-
+int multiplicar(int a, int b);
+float dividir(int a, int b);
 int main() {
     int num1, num2;
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &num1);
-    printf("Digite o segundo numero: ");
-    scanf("%d", &num2);
+    printf("Digite dois numeros inteiros (ex 20 30): ");
+    scanf("%d %d", &num1, &num2);
     
-    // Soma
+    // Chamada das funções e exibição dos resultados
     printf("Soma: %d\n", soma(num1, num2));
-    // Subtracao
-printf("Subtracao: %d\n", subtrair(num1, num2));
-return 0;
+    printf("Subtracao: %d\n", subtrair(num1, num2));
+    printf("Multiplicacao: %d\n", multiplicar(num1, num2));
+    printf("Divisao: %.2f\n", dividir(num1, num2)); 
+    return 0;
 }
-// Implementacao da funcao
-int soma (int a, int b) {
+// Implementação das funções
+int soma(int a, int b) {
     return a + b;
 }
 int subtrair(int a, int b) {
     return a - b;
+}
+int multiplicar(int a, int b) {
+    return a * b;
+}
+float dividir(int a, int b) {
+    return (float)a / b;
 }
